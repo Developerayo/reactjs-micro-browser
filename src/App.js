7438ce3e3e4dfd1774e108cd981950847ad58ab1
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-04-03 17:58:24 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <h4>Micro Browsers</h4>
-  <small>Unfortunately this only works for IOS devices, <br/>  Copy and paste the link to this site on imessage on IOS device to view the demo.</small>
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					<h4>Micro Browsers</h4>
+					<small>
+						Unfortunately this only works for IOS devices, <br /> Copy and paste the
+						link to this site on imessage on IOS device to view the demo.
+					</small>
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
