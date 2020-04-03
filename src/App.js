6c3652cd35132c2acbfa9791e8ee3eea50cc832e
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-04-03 17:56:27 +0100

    add a word

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <p>
           <h4>Micro Browsers</h4>
-  <small>Unfortunately this only works for IOS devices, <br/>  Copy and paste the link to this site on imessage on IOS to view the demo.</small>
+  <small>Unfortunately this only works for IOS devices, <br/>  Copy and paste the link to this site on imessage on IOS device to view the demo.</small>
         </p>
       </header>
     </div>
